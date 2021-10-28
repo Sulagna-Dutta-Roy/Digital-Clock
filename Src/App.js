985a60fbc43ce5bc6a2e33a8fc1dff9fef4e1957
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2021-10-29 01:09:19 +0530

    Add files via upload

---
+++ Src/App.js
@@ -0,0 +1,20 @@
+import React,{useState} from 'react'
+
+const App = () => {
+  let time  = new Date().toLocaleTimeString();
+
+  const [Ctime, setCtime] = useState(time);
+
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCtime(time);
+  }
+  setInterval(updateTime,1000);
+
+  return (
+    <>
+       <h1><span className="center">{time}</span></h1>
+    </>
+  );
+}
+export default App;
